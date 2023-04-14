d51fc57ca852c9f3d7748c3d4a539ab8bcd386f3
Chia Chung <limchiachung0611@gmail.com>
2023-04-14 08:34:32 +0800

    Update Method-with-ClassMethod-and-StaticMethod.py

--- 2. Method/Method-with-ClassMethod-and-StaticMethod.py
+++ 2. Method/Method-with-ClassMethod-and-StaticMethod.py
@@ -1,3 +1,12 @@
+"""
+The code defines a Person class with two attributes and two methods: a constructor (init), a class method
+(from_birth_year), and a static method (get_age). The class method creates a new instance of the Person class based on
+a birth year input, while the static method calculates the age based on the current year and the birth year input.
+The code creates two instances of the Person class and prints their names and ages. The code is well-written and uses
+class methods and static methods effectively but could benefit from additional methods and error handling.
+"""
+
+
 class Person:
     def __init__(self, name, age):
         self.name = name
