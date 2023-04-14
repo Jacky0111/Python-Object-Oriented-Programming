5db5ea158058d0bde17c00a0a194cd633d936f6f
Chia Chung <limchiachung0611@gmail.com>
2023-04-14 08:34:36 +0800

    Update polymorphism-1.py

--- 6. Polymorphism/polymorphism-1.py
+++ 6. Polymorphism/polymorphism-1.py
@@ -1,3 +1,14 @@
+"""
+The code defines a Fruit class with an abstract method eat and two subclasses, Apple and Banana, which implement
+their own eating behavior. It also defines a function eat_fruit that takes a fruit object and calls its eat method.
+The code creates instances of the Apple and Banana classes and passes them as arguments to the eat_fruit function,
+which calls their respective eat methods and prints their eating behavior.
+
+Overall, the code demonstrates the use of inheritance and polymorphism to define a general class with specific
+behavior for its subclasses. However, the code could be improved by adding error handling for invalid input values.
+"""
+
+
 class Fruit:
     def __init__(self, name):
         self.name = name
@@ -25,7 +36,6 @@ banana = Banana("Cavendish")
 
 eat_fruit(apple)
 eat_fruit(banana)
-
 
 '''
 Output
