595365fc66a4d9f6ac07b84d4d7d24ca4c0a0616
Chia Chung <limchiachung0611@gmail.com>
2023-04-14 08:37:46 +0800

    Update encapsulation-1.py

--- 5. Encapsulation/encapsulation-1.py
+++ 5. Encapsulation/encapsulation-1.py
@@ -1,3 +1,11 @@
+"""
+This program defines a BankAccount class that has methods for getting the balance, depositing and withdrawing
+money. An instance of this class is created with an initial balance of 1000. The balance is then printed and 500 is
+deposited and the balance is printed again. Finally, 200 is withdrawn and the balance is printed again. The output
+shows the balance after each of these operations.
+"""
+
+
 class BankAccount:
     def __init__(self, balance):
         self._balance = balance
