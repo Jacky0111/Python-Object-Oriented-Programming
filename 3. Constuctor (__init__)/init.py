91fd47c109fbb48a53ad3747502a3d0d25343462
Chia Chung <limchiachung0611@gmail.com>
2023-04-14 08:34:24 +0800

    Update init.py

--- 3. Constuctor (__init__)/init.py
+++ 3. Constuctor (__init__)/init.py
@@ -1,3 +1,11 @@
+"""
+The code defines a Counter class with a constructor (init method) that initializes the count variable to zero and
+a method (increment) that increments the count variable by 1. The code creates an instance of the Counter class,
+increments the count variable twice, and prints the initial and updated values of the count variable. The code is
+well-written but could benefit from additional methods and error handling.
+"""
+
+
 class Counter:
     def __init__(self):
         self.count = 0
