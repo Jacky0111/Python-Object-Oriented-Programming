5313684af3f41520ba4008f1900fa87064c5479e
Chia Chung <limchiachung0611@gmail.com>
2023-04-14 08:34:21 +0800

    Update Class-Attributes.py

--- 1. Class/Class-Attributes.py
+++ 1. Class/Class-Attributes.py
@@ -1,3 +1,13 @@
+"""
+The initial value of score is 100, which is the class attribute. When an instance of the Student class is created
+and its score attribute is accessed, the value of the class attribute (100) is returned.
+
+When the setScore method of the instance s is called, it sets the score attribute of that instance to 99, which is an
+instance attribute. When the score attribute of s is accessed again, it returns the value of the instance attribute,
+which is 99.
+"""
+
+
 class Student:
     score = 100  # Class Attribute
 
